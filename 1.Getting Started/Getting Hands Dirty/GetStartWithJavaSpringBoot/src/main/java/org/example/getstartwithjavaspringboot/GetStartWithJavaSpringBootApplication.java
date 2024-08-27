68934b06e280c011d033956d8bb8c6ddb03b0bdb
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-08-28 06:46:06 +0700

    PUSH my first project with docker

---
+++ 1.Getting Started/Getting Hands Dirty/GetStartWithJavaSpringBoot/src/main/java/org/example/getstartwithjavaspringboot/GetStartWithJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.getstartwithjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetStartWithJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetStartWithJavaSpringBootApplication.class, args);
+	}
+
+}
